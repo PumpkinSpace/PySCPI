00c47ccb8089831af53fde37882b23c94ebf260f
djwnz <djwright@stanford.edu>
2016-11-15 10:29:27 -0800

    better string formatting, removed write/read declarations added bsm commands

--- SCPI_Commands.py
+++ SCPI_Commands.py
@@ -19,31 +19,33 @@ from struct import unpack
 
 # constants that define the structure of telemetry data
 name_size = 32
-chksum_size = 2
+chksum_size = 0
 wflag_size = 1
 time_size = 4
 length_size = 1
 
 # Dictionary of all telemetry Commands
 SCPI_Data = {
+    # perscribed format:
+    #'SCPI:COMMAND':      [length of data to be read,                            'format of the data'],
     ############################### SupMCU Commands #####################################
     # SupMCU Version
     'SUP:TEL? 0,data':    [wflag_size + time_size + chksum_size + 48,            'ascii'],
     'SUP:TEL? 0,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 0,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 0,ascii':   [wflag_size + time_size + chksum_size + 48,            'ascii'],
+    'SUP:TEL? 0,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     # SCPI Commands Parsed
     'SUP:TEL? 1,data':    [wflag_size + time_size + chksum_size + 8,             'long long'],
     'SUP:TEL? 1,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 1,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 1,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],
+    'SUP:TEL? 1,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     # SCPI Command Errors
     'SUP:TEL? 2,data':    [wflag_size + time_size + chksum_size + 1,             'uint'],
     'SUP:TEL? 2,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 2,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 2,ascii':   [wflag_size + time_size + chksum_size + 1,             'ascii'],    
+    'SUP:TEL? 2,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],    
     
     # Voltstat
     'SUP:TEL? 3,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
@@ -52,74 +54,117 @@ SCPI_Data = {
     'SUP:TEL? 4,data':    [wflag_size + time_size + chksum_size + 22,            'long, long, int, int, int'],
     'SUP:TEL? 4,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 4,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 4,ascii':   [wflag_size + time_size + chksum_size + 22,            'ascii'],    
+    'SUP:TEL? 4,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],    
     
     # Elapsed Time in Seconds
     'SUP:TEL? 5,data':    [wflag_size + time_size + chksum_size + 8,             'long long'],
     'SUP:TEL? 5,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 5,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 5,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],    
+    'SUP:TEL? 5,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],    
     
     # Number of context switches
     'SUP:TEL? 6,data':    [wflag_size + time_size + chksum_size + 8,             'long long'],
     'SUP:TEL? 6,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 6,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 6,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],       
+    'SUP:TEL? 6,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],       
     
     # idling Hooks remaining
     'SUP:TEL? 7,data':    [wflag_size + time_size + chksum_size + 8,             'long long'],
     'SUP:TEL? 7,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 7,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 7,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],      
+    'SUP:TEL? 7,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],      
     
     # MCU Load
     'SUP:TEL? 8,data':    [wflag_size + time_size + chksum_size + 8,             'double'],
     'SUP:TEL? 8,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'SUP:TEL? 8,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'SUP:TEL? 8,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],
+    'SUP:TEL? 8,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     ############################### PIM Commands ########################################
     # Channel Currents
     'PIM:TEL? 0,data':    [wflag_size + time_size + chksum_size + 8,             'uint, uint, uint, uint'],
     'PIM:TEL? 0,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 0,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 0,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],
+    'PIM:TEL? 0,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     # Shunt Resistor Values
     'PIM:TEL? 1,data':    [wflag_size + time_size + chksum_size + 8,             'uint, uint, uint, uint'],
     'PIM:TEL? 1,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 1,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 1,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],
+    'PIM:TEL? 1,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     # Channel Current Limits
     'PIM:TEL? 2,data':    [wflag_size + time_size + chksum_size + 8,             'uint, uint, uint, uint'],
     'PIM:TEL? 2,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 2,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 2,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii'],   
+    'PIM:TEL? 2,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],   
     
     # Channel Current offsets for linear fit
     'PIM:TEL? 3,data':    [wflag_size + time_size + chksum_size + 32,            'double, double, double, double'],
     'PIM:TEL? 3,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 3,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 3,ascii':   [wflag_size + time_size + chksum_size + 32,            'ascii'],  
+    'PIM:TEL? 3,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],  
     
     # Channel Curretn scaling factors for linear fit
     'PIM:TEL? 4,data':    [wflag_size + time_size + chksum_size + 32,            'double, double, double, double'],
     'PIM:TEL? 4,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 4,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 4,ascii':   [wflag_size + time_size + chksum_size + 32,            'ascii'],    
+    'PIM:TEL? 4,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],    
     
     # Status Register
     'PIM:TEL? 5,data':    [wflag_size + time_size + chksum_size + 1,             'hex'],
     'PIM:TEL? 5,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 5,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 5,ascii':   [wflag_size + time_size + chksum_size + 1,             'ascii'],
+    'PIM:TEL? 5,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
     
     # Channel over current event log
     'PIM:TEL? 6,data':    [wflag_size + time_size + chksum_size + 8,             'uint, uint, uint, uint'],
     'PIM:TEL? 6,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
     'PIM:TEL? 6,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
-    'PIM:TEL? 6,ascii':   [wflag_size + time_size + chksum_size + 8,             'ascii']    
+    'PIM:TEL? 6,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
+    
+    ############################### BSM Commands ########################################
+    # Channel Currents
+    'BSM:TEL? 0,data':    [wflag_size + time_size + chksum_size + 10,            'uint, uint, uint, uint, uint'],
+    'BSM:TEL? 0,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 0,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 0,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],  
+    
+    # Channel Shunt resistor values
+    'BSM:TEL? 1,data':    [wflag_size + time_size + chksum_size + 10,            'uint, uint, uint, uint, uint'],
+    'BSM:TEL? 1,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 1,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 1,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],     
+    
+    # Channel Current limits
+    'BSM:TEL? 2,data':    [wflag_size + time_size + chksum_size + 10,            'uint, uint, uint, uint, uint'],
+    'BSM:TEL? 2,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 2,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 2,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],  
+    
+    # Channel Current offsets for linear fit
+    'BSM:TEL? 3,data':    [wflag_size + time_size + chksum_size + 40,            'double, double, double, double, double'],
+    'BSM:TEL? 3,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 3,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 3,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
+    
+    # Channel Current scaling factors for linear fit
+    'BSM:TEL? 4,data':    [wflag_size + time_size + chksum_size + 40,            'double, double, double, double, double'],
+    'BSM:TEL? 4,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 4,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 4,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],
+    
+    # Status register
+    'BSM:TEL? 5,data':    [wflag_size + time_size + chksum_size + 1,             'char'],
+    'BSM:TEL? 5,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 5,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 5,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'], 
+    
+    # Channel over current event log
+    'BSM:TEL? 6,data':    [wflag_size + time_size + chksum_size + 10,            'uint, uint, uint, uint, uint'],
+    'BSM:TEL? 6,name':    [wflag_size + time_size + name_size + chksum_size,     'ascii'],
+    'BSM:TEL? 6,length':  [wflag_size + time_size + length_size + chksum_size,   'char'],
+    'BSM:TEL? 6,ascii':   [wflag_size + time_size + chksum_size + 128,           'ascii'],     
     }
 
 
@@ -146,15 +191,24 @@ def print_read(command, raw_data):
         print_format = SCPI_Data[command][1]
         
         # split the incoming command into its respective parts
-        write_flag = raw_data[0:wflag_size-1]
+        write_flag = raw_data[0:wflag_size]
         timestamp = raw_data[wflag_size:wflag_size+time_size]
-        data = raw_data[wflag_size+time_size:-chksum_size]
-        checksum = raw_data[-chksum_size:]
         
-        if write_flag == 0:
+        # index to stop printing at
+        stop_index = len(raw_data)
+        
+        
+        if chksum_size != 0:
+            checksum = raw_data[-chksum_size:]
+            data = raw_data[wflag_size+time_size:-chksum_size]
+        else:
+            data = raw_data[wflag_size+time_size:]
+        # end
+        
+        if write_flag[0] == 0:
             # The command was sent too fast, bad data was recieved
             print '*** Read failed, Write flag = 0 ***'
-            return 0
+            
         # else the data is good
         elif ',' not in print_format:
             # the data is just a single peice of data, not a list.
@@ -164,7 +218,8 @@ def print_read(command, raw_data):
  
             # print the data in the appropriate format given by the dictionary
             if print_format == 'ascii':
-                print ''.join([chr(x) for x in data])
+                print ''.join([chr(x) for x in data[0:data.index(0)]])
+                stop_index = data.index(0) + 5
                 
             elif print_format == 'int':
                 print unpack('<h', ''.join([chr(x) for x in data]))[0]
@@ -179,7 +234,7 @@ def print_read(command, raw_data):
                 print unpack('<H', ''.join([chr(x) for x in data]))[0]  
                 
             elif print_format == 'double':
-                print unpack('>d', ''.join([chr(x) for x in data]))[0]
+                print unpack('<d', ''.join([chr(x) for x in data]))[0]
                 
             elif print_format == 'char':
                 print unpack('<B', ''.join([chr(x) for x in data]))[0] 
@@ -191,6 +246,12 @@ def print_read(command, raw_data):
                 # the format in the dictionary does not match one of the supported types
                 print '*** No valid format for data ***'
             # end
+            
+            # print checksum if present
+            if chksum_size != 0:
+                print 'Checksum: ' + ' '.join(['0x%02x' % x for x in checksum])
+            # end
+            
         else:
             # the data received is a list so process each piece individually
             # does not accept hex or ascii parts in the list
@@ -222,7 +283,7 @@ def print_read(command, raw_data):
                     start_index += 2
                     
                 elif spec == 'double':
-                    output[i] =  unpack('>d', ''.join([chr(x) for x in data[start_index:start_index+8]]))[0]
+                    output[i] =  unpack('<d', ''.join([chr(x) for x in data[start_index:start_index+8]]))[0]
                     start_index += 8
                     
                 elif spec == 'char':
@@ -236,11 +297,16 @@ def print_read(command, raw_data):
                 i += 1
             # end
             print output
+            
+            # print checksum if present
+            if chksum_size != 0:
+                print 'Checksum: ' + ' '.join(['0x%02x' % x for x in checksum])
+            # end            
         #end
     else:
         print '*** This command is not in the library ***\nHex:'
     # end
     
     # print the Hex data at the end, also print in hex by default
-    print 'Hex:\n' + ' '.join(['%02x' % x for x in raw_data])
+    print 'Hex:\n' + ' '.join(['%02x' % x for x in raw_data[0:stop_index]])
 # end
--- aardvark builder.py
+++ aardvark builder.py
@@ -22,7 +22,7 @@ Bitrate = 100
 filename = 'aardvark_script.xml'
 
 # Intermessage delay in milliseconds
-Delay = 100
+Delay = 300
 
 # I2C address dictionary
 address_of = {'PIM':        '0x53',
@@ -43,25 +43,17 @@ address = address_of['PIM']
 dec_addr = int(address,0)
 
 ########################## SCPI Commands #######################################
-# commands is a list of read/write commands and the SCPI command to read/write
-# a command takes the form of a pair of list entries the read/write command
-# taking the even entry in the list and the associated SCPI commad taking the 
-# even entry.
-# For a write command use the string 'WRITE' as the even item in the list and
-# then use the string of the command as the odd item eg. 'SUP:LED ON'
-# For a read command the even entry is a string 'READ ' followed by the number 
-# of bytes to read, ie. to read 10 bytes it would be 'READ 10'. The odd entry is
-# as before just the SCPI command string eg. 'SUP:TEL? 2,length'
-#
+# commands is a list of SCPI Commands to be sent, TELEM requests will have all
+# appropriate reading steps done following sending the command
 #
 # Example:
-# commands = ['WRITE',    'SUP:LED ON',
-#             'READ 21',  'SUP:TEL? 2,length'] 
+# commands = ['SUP:LED ON',
+#             'SUP:TEL? 2,length'] 
 
-commands = ['WRITE',     'PIM:PORT:POW ON,4',
-            'READ 39',   'PIM:TEL? 1,data',
-            'READ 39',   'PIM:TEL? 3,data',
-            'READ 39',   'PIM:TEL? 4,data',
+commands = ['SUP:TEL? 0,name',
+            'SUP:TEL? 0,length',
+            'SUP:TEL? 0,data',
+            'SUP:TEL? 0,ascii',
             ]
             
 ################################################################################
@@ -77,6 +69,7 @@ Aardvark_port = aa_find_devices(1)[1][0]
 Aardvark_free = True
 if Aardvark_port >= 8<<7:
     print ' *** Aardvark is being used ***'
+    aa_close(Aardvark_port)
     Aardvark_free = False
 else:
     # Aardvark is available so configure it
@@ -119,75 +112,61 @@ while i < len(commands):
     ET.SubElement(aardvark, 'sleep', delay_attributes)
     
     # comment the string of the SCPI command
-    aardvark.append(ET.Comment(commands[i+1]))
+    aardvark.append(ET.Comment(commands[i]))
     
-    # determine the type of command requested
-    if ((commands[i] == 'WRITE') or (commands[i].startswith('READ'))):
-        # Command is a valid write or read command
-        
-        # define attributes for write element
-        write_attributes = {'addr':  address,
-                            'count': str(len(commands[i+1])+1),
-                            'radix': str(radix)}
-        
-        # create write element
-        scpi = ET.SubElement(aardvark, 'i2c_write', write_attributes)
-        
-        # add hexidecimal null terminated command as text to the write element
-        scpi.text = ' '.join("{:02x}".format(ord(c)) for c in commands[i+1]) + ' 0a'
-        
-        # See if the Aardvark is free
-        if Aardvark_free:
-            # Prepare the data for transmission
-            write_data = list(commands[i+1])
-            write_data = [ord(item) for item in write_data]
-            write_data.append(0x0a)
-            data = array('B', write_data)  
-            # Write the data to the slave device
-            aa_i2c_write(Aardvark_in_use, dec_addr, AA_I2C_NO_FLAGS, data)
-            aa_sleep_ms(Delay)
-            if not (commands[i].startswith('READ')):
-                print 'Write: ' + commands[i+1]
-            else:
-                print 'Read: ' + commands[i+1]
-            # end
+    # define attributes for write element
+    write_attributes = {'addr':  address,
+                        'count': str(len(commands[i])+1),
+                        'radix': str(radix)}
+    
+    # create write element
+    scpi = ET.SubElement(aardvark, 'i2c_write', write_attributes)
+    
+    # add hexidecimal null terminated command as text to the write element
+    scpi.text = ' '.join("{:02x}".format(ord(c)) for c in commands[i]) + ' 0a'
+    
+    # See if the Aardvark is free
+    if Aardvark_free:
+        # Prepare the data for transmission
+        write_data = list(commands[i])
+        write_data = [ord(item) for item in write_data]
+        write_data.append(0x0a)
+        data = array('B', write_data)  
+        # Write the data to the slave device
+        aa_i2c_write(Aardvark_in_use, dec_addr, AA_I2C_NO_FLAGS, data)
+        aa_sleep_ms(Delay)
+        if 'TEL?' in commands[i]:
+            print 'Read: ' + commands[i]
+        else:
+            print 'Write: ' + commands[i]
         # end
-        
-        if (commands[i].startswith('READ')):
-            # Read command was issued so a read needs to be performed
-            ET.SubElement(aardvark, 'sleep', delay_attributes)
-            
-            # define attributes for read element extracting length from command
-            read_attributes = {'addr':  address,
-                               'count': commands[i].split(' ')[1],
-                               'radix': str(radix)}        
-            
-            # check the length of the command desired
-            if int(read_attributes['count']) != read_length(commands[i+1]):
-                aardvark.append(ET.Comment('*** requested length does not match that specified in dictionary ***'))
-                print '*** requested length does not match that specified in dictionary ***'
-            # end
-            
-            # create the read element
-            read = ET.SubElement(aardvark, 'i2c_read', read_attributes) 
-            
-            # if the Aardvark is free read from it
-            if Aardvark_free:
-                data = array('B', [1]*read_length(commands[i+1])) 
-                # read from the slave device
-                read_data = aa_i2c_read(Aardvark_in_use, dec_addr, AA_I2C_NO_FLAGS, data)
-                print_read(commands[i+1], list(read_data[1]))
-                aa_sleep_ms(Delay)
-            # end  
-        # end
-        print ''
-    else:
-        # command was not valid
-        print('Error in read/write declaration')
     # end
     
+    if 'TEL?' in commands[i]:
+        # Read command was issued so a read needs to be performed
+        ET.SubElement(aardvark, 'sleep', delay_attributes)
+        
+        # define attributes for read element extracting length from command
+        read_attributes = {'addr':  address,
+                           'count': str(read_length(commands[i])),
+                           'radix': str(radix)}        
+            
+        # create the read element
+        read = ET.SubElement(aardvark, 'i2c_read', read_attributes) 
+        
+        # if the Aardvark is free read from it
+        if Aardvark_free:
+            data = array('B', [1]*read_length(commands[i])) 
+            # read from the slave device
+            read_data = aa_i2c_read(Aardvark_in_use, dec_addr, AA_I2C_NO_FLAGS, data)
+            print_read(commands[i], list(read_data[1]))
+            aa_sleep_ms(Delay)
+        # end  
+    # end
+    print ''
+    
     # Iterate to next command
-    i+=2
+    i+=1
 #end loop
 
 
